332f94dee89cec0042b10b4a62d7efd0c0f4a3dd
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2025-11-19 10:48:39 +0200

    Creating Routes & Controllers

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require("express");
+const tasksRouter = require("./routes/tasks");
+const app = express();
+
+// Middleware
+app.use(express.json()); // to conver the req.body from raw JSON (Undifiend for Express) -> Object
+
+//        root route       Router
+app.use("/api/v1/tasks", tasksRouter);
+
+const port = 3000;
+app.listen(port, console.log(`Server is listening on port ${port}...`));
